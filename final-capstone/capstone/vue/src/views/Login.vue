77d06b4af497a2fd345a82c5c211d790a207b004
Kevin Little <littlekt12@gmail.com>
2023-05-31 11:22:20 -0400

    Login page

--- final-capstone/capstone/vue/src/views/Login.vue
+++ final-capstone/capstone/vue/src/views/Login.vue
@@ -1,5 +1,7 @@
 <template>
+<div>
   <div id="login">
+    <div class="image"> </div>
     <form @submit.prevent="login">
       <h1 >Please Sign In</h1>
       <div role="alert" v-if="invalidCredentials">
@@ -9,18 +11,24 @@
         Thank you for registering, please sign in.
       </div>
       <div class="form-input-group">
-        <label for="username">Username</label>
+        <label for="username">Username </label>
         <input type="text" id="username" v-model="user.username" required autofocus />
       </div>
       <div class="form-input-group">
-        <label for="password">Password</label>
+        <label for="password">Password </label>
         <input type="password" id="password" v-model="user.password" required />
       </div>
-      <button type="submit">Sign in</button>
+      <button type="submit" class="signin">Sign In</button>
       <p>
-      <router-link :to="{ name: 'register' }">Need an account? Sign up.</router-link></p>
+      <router-link :to="{ name: 'register' }" class="register-link">Need an account? Sign up.</router-link></p>
     </form>
+    <div class="image2"> </div>
   </div>
+  <div class = "bottom-image">
+
+  </div>
+</div>
+   
 </template>
 
 <script>
@@ -65,7 +73,75 @@ export default {
 .form-input-group {
   margin-bottom: 1rem;
 }
+input {
+  font-size: 15px;
+}
 label {
   margin-right: 0.5rem;
 }
+a.register-link {
+  color: gold;
+}
+.signin {
+  width: 30%;
+  font-family: 'Bangers', cursive;
+  font-size: 25px;
+}
+form{
+  font-size: 30px;
+  width: 35%;
+  border: 2px solid gray; /* Add border */
+  border-radius: 10px; /* Optional: Add border radius for rounded corners */
+  padding: 10px;
+  background-color: rgba(150, 150, 150, 0.37);
+}
+#login {
+  display: flex;
+  flex-direction: space-between;
+  position: relative;
+  text-align: center;
+  font-family: 'Bangers', cursive;
+  height: 50vh;
+  width: 100%;
+  
+}
+/* .container { 
+  border: gray transparent background;
+} */
+
+.image {
+  background-image: url('../doom.png');
+  height: 100%;
+  width: 33%;
+  background-repeat: no-repeat;
+    background-position: top center;
+}
+.image2 {
+  background-image: url('../darkseidfinal.png');
+  height: 100%;
+  width: 33%;
+  background-repeat: no-repeat;
+  background-position: top center;
+}
+.bottom-image {
+  background-image: url('../thejokababy.png');
+  height: 50vh;
+  width: 100%;
+  background-repeat: no-repeat;
+  background-position: bottom center;
+  background-size: 35%;
+}
+  @media only screen and (max-width: 600px) {
+    .image {
+      background-size: contain;
+    }
+    .image2 {
+      background-size: contain;
+    }
+    .bottom-image {
+      background-size: contain;
+    }
+  }
+
+
 </style>
