d0285c143816df032a2789ff25ae210e2420e261
Thomas Chavez <thomaschavez981@hotmail.com>
2023-06-05 07:11:42 -0700

    Saving before merge

--- final-capstone/capstone/vue/src/views/CollectionDetail.vue
+++ final-capstone/capstone/vue/src/views/CollectionDetail.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="collection-detail">
     <h1>{{ collection.name }}</h1>
+    
+    <!-- Add Comic Form -->
     <form @submit.prevent="addComic" class="add-comic-form">
       <input type="text" v-model="newComicName" placeholder="Enter comic name" required />
       <button type="submit">Add Comic</button>
     </form>
+    
+    <!-- Share Collection URL -->
+    <div class="share-collection">
+      <p>Share this collection:</p>
+      <input type="text" :value="collectionURL" readonly />
+    </div>
+    
+    <!-- Comic Grid -->
     <div class="comic-grid">
       <div v-for="comic in collection.comics" :key="comic.id" class="comic-item">
-        <img src="https://via.placeholder.com/200" alt="Placeholder Image" />
+          <img src="https://m.media-amazon.com/images/I/71GPeneF0rL._AC_UF894,1000_QL80_.jpg" alt="Comic Image" />
         <p>Comic Name</p>
       </div>
     </div>
   </div>
 </template>
+
 
 <script>
 export default {
@@ -24,6 +35,14 @@ export default {
       },
       newComicName: '',
     };
+  },
+  computed: {
+    collectionURL() {
+      // Get the current route and build the collection URL
+      const currentRoute = this.$route.fullPath;
+      const collectionID = this.collection.id; // Replace with the actual collection ID
+      return `${window.location.origin}${currentRoute}?collectionId=${collectionID}`;
+    },
   },
   methods: {
     addComic() {
