a1c698c39d68cf464c7c2fb723f6cce430b8f8dc
Sean Goldsmith <goldsmith.sean@gmail.com>
2023-06-06 18:16:14 -0400

    added requestmapping and API call for date and issue search

--- final-capstone/capstone/java/src/main/java/com/techelevator/controller/APIRequestController.java
+++ final-capstone/capstone/java/src/main/java/com/techelevator/controller/APIRequestController.java
@@ -2,8 +2,7 @@ package com.techelevator.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.techelevator.dao.CollectionJdbcDao;
-import com.techelevator.model.CharacterComicData;
-import com.techelevator.model.Comic;
+import com.techelevator.model.*;
 import com.techelevator.services.MetronCloud;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -26,10 +25,21 @@ public class APIRequestController {
 
     @RequestMapping(path="/comic-character", method= RequestMethod.GET)
     public List<CharacterComicData> comics(@RequestParam String comicSearch) throws JsonMappingException, JsonProcessingException {
-
         return comicAPIRequest.getComicResults(comicSearch);
-
     }
+
+    //TODO change path to match vue
+    @RequestMapping(path="/CHANGE ME", method= RequestMethod.GET)
+    public ComicSpecificIssue comic(@RequestParam int issueSearch) throws JsonMappingException, JsonProcessingException {
+        return comicAPIRequest.getComicById(issueSearch);
+    }
+
+    @RequestMapping(path="/CHANGE ME", method= RequestMethod.GET)
+    public List<ResultByDate> comics(@RequestParam Integer month, Integer date) throws JsonMappingException, JsonProcessingException {
+        return comicAPIRequest.getComicByDate(month, date);
+    }
+
+
 }
 
 
--- final-capstone/capstone/java/src/main/java/com/techelevator/services/MetronCloud.java
+++ final-capstone/capstone/java/src/main/java/com/techelevator/services/MetronCloud.java
@@ -2,7 +2,6 @@ package com.techelevator.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.techelevator.model.*;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,7 +9,6 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 
     //TODO CHECK application.properties.
@@ -34,8 +32,6 @@ public class MetronCloud {
         //TODO NEEDS DISCUSSED HOW WE WANT TO PULL THINGS
         String url = this.searchURL + "character/?name=" + comicSearchEntry;
 
-
-
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setBasicAuth(this.username, this.password);
@@ -57,18 +53,54 @@ public class MetronCloud {
             ResponseEntity<String> comicResponse = restTemplate.exchange(urlComicSearch, HttpMethod.GET, httpEntity, String.class);
             CharacterData characterData = objectMapper.readValue(comicResponse.getBody(), CharacterData.class);
             return characterData.getResults();
-
-
         }
-
-
-
-
-
         return null;
     }
 
+    public List<ResultByDate> getComicByDate(Integer month, Integer year) throws JsonMappingException, JsonProcessingException {
+
+
+        //TODO NEEDS DISCUSSED HOW WE WANT TO PULL THINGS
+        String url = this.searchURL + "/issue/?cover_month=" + month + "&cover_year=" + year;
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth(this.username, this.password);
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        IssueByDate comicByDate = objectMapper.readValue(response.getBody(), IssueByDate.class);
+        List<ResultByDate> resultsByDate = comicByDate.getResultsByDate();
+        String searchDate = year + "-" + month;
+        for (ResultByDate mth : resultsByDate) {
+            if (mth.getCoverDate().substring(0, 8).equals(searchDate)) {
+                resultsByDate.add(mth);
+            }
+        }
+        return resultsByDate;
     }
+
+    public ComicSpecificIssue getComicById(int comicId) throws JsonMappingException, JsonProcessingException {
+
+
+        //TODO NEEDS DISCUSSED HOW WE WANT TO PULL THINGS
+        String url = this.searchURL + "issue/" + comicId + "/";
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth(this.username, this.password);
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ComicSpecificIssue indIssue = objectMapper.readValue(response.getBody(), ComicSpecificIssue.class);
+        return indIssue;
+
+    }
+}
 
 
 
