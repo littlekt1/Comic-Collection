b6e1aff2ed15ee66b9128bdeb0039b42062ceecd
Thomas Chavez <thomaschavez981@hotmail.com>
2023-06-06 07:57:20 -0700

    Explore page search bar

--- final-capstone/capstone/vue/src/views/ExplorePage.vue
+++ final-capstone/capstone/vue/src/views/ExplorePage.vue
@@ -1,14 +1,63 @@
 <template>
   <div class="explore-page">
     <h1>Explore Comics</h1>
-    <router-link to="/bulk-add-comics">Bulk Add Comics</router-link>
-    <!-- Other content of the Explore page -->
+
+    <!-- Search Bar -->
+    <div class="search-bar">
+      <input type="text" v-model="searchQuery" placeholder="Search by name, author, or character" />
+      <button @click="searchComics">Search</button>
+    </div>
+
+    <!-- Comic Grid -->
+    <div class="comic-grid">
+      <div v-for="comic in searchResults" :key="comic.id" class="comic-item">
+        <img src="https://via.placeholder.com/200" alt="Placeholder Image" />
+        <p>{{ comic.name }}</p>
+        <p>Author: {{ comic.author }}</p>
+        <p>Characters: {{ comic.characters.join(', ') }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  // Component logic
+  data() {
+    return {
+      searchQuery: '',
+      searchResults: [],
+      comics: [
+        { id: 1, name: 'Comic A', author: 'Author A', characters: ['Superman', 'Spider-Man'] },
+        { id: 2, name: 'Comic B', author: 'Author B', characters: ['Batman', 'Superman'] },
+        { id: 3, name: 'Comic C', author: 'Author C', characters: ['Spider-Man', 'Wonder Woman'] },
+        // Add more comics as needed
+      ],
+    };
+  },
+  methods: {
+   searchComics() {
+  const query = this.searchQuery.trim().toLowerCase();
+
+  if (query === '') {
+    this.searchResults = [];
+    return;
+  }
+
+  // Filter the comics based on the search query
+  this.searchResults = this.comics.filter((comic) => {
+    const comicName = comic.name.toLowerCase();
+    const author = comic.author.toLowerCase();
+    const characters = comic.characters.map((character) => character.toLowerCase());
+
+    // Check if the search query matches the comic name, author, or any character
+    return (
+      comicName.includes(query) ||
+      author.includes(query) ||
+      characters.some((character) => character.includes(query))
+    );
+  });
+    },
+  },
 };
 </script>
 
@@ -17,12 +66,42 @@ export default {
   text-align: center;
 }
 
-.explore-page h1 {
-  margin-bottom: 20px;
+.search-bar {
+  margin: 20px 0;
 }
 
-.explore-page router-link {
-  display: block;
-  margin-bottom: 10px;
+.search-bar input[type="text"] {
+  border: 2px solid black;
+  padding: 8px;
+  font-size: 16px;
+}
+
+.search-bar button {
+  font-size: 16px;
+  padding: 8px 16px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+.comic-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 20px;
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.comic-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.comic-item img {
+  width: 150px;
+  max-height: 200px;
+  object-fit: cover;
 }
 </style>
